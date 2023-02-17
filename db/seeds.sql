576f176b81fb60e1b2c9a786e800e6decb6a09e9
SeanAllen19 <saallen19.dev@gmail.com>
2023-02-16 21:36:32 -0600

    Reach my goal with this project

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,13 @@
-INSERT INTO department (id, name) VALUES (1, 'Managment');
-INSERT INTO department (id, name) VALUES (2, 'Sales');
-INSERT INTO department (id, name) VALUES (3, 'Finance');
-INSERT INTO department (id, name) VALUES (4, 'Kitchen');
+INSERT INTO department ( name) VALUES ('Managment');
+INSERT INTO department ( name) VALUES ( 'Sales');
+INSERT INTO department ( name) VALUES ( 'Finance');
+INSERT INTO department ( name) VALUES ('Kitchen');
 
-
-INSERT INTO role (id,title, salary, department_id) VALUES (10,"Head Engineer", 150000, 1);
-INSERT INTO role (id,title, salary, department_id) VALUES (20,"Senior Mgr.", 138500, 2);
-INSERT INTO role (id,title, salary, department_id) VALUES (30,"Head Chef", 110000, 10);
+INSERT INTO role (title, salary, department_id) VALUES ("Head Engineer", 150000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Senior Mgr.", 138500, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Head Chef", 110000, 1);
         
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (001,'Sean', 'Allen', 'Senior', 10 );
-INSERT INTO employee (id, first_name, last_name, role_id, ) VALUES (002, 'Mark', 'Young', 2, 10);
-INSERT INTO employee (id, first_name, last_name, role_id, ) VALUES (003,'Leslie', 'Of The 6th', 3, null);
-INSERT INTO employee (id, first_name, last_name, role_id, ) VALUES (004, 'Justin', 'Caya', 4, null);
+INSERT INTO employee ( first_name, last_name, role_id, manager_id) VALUES ('Sean', 'Allen', 1, null );
+INSERT INTO employee ( first_name, last_name, role_id, manager_id) VALUES ('Mark', 'Young', 2, 2);
+INSERT INTO employee ( first_name, last_name, role_id, manager_id) VALUES ('Leslie', 'Of The 6th', 3, null);
+INSERT INTO employee ( first_name, last_name, role_id, manager_id) VALUES ('Justin', 'Caya', 4, 4);
--- index.js
+++ index.js
@@ -5,15 +5,15 @@ require("console.table");
 
 const connection = mysql.createConnection(
     {
-      host: 'localhost',
-      user: 'root',
-      // MySQL password
-      password: '123password',
-      database: 'employees_db',
-      
+        host: 'localhost',
+        user: 'root',
+        // MySQL password
+        password: '123password',
+        database: 'employees_db',
+
     },
-  );
-  
+);
+
 connection.connect(
 
     console.log(`Connected to the classlist_db database.`),
@@ -24,186 +24,304 @@ connection.connect(
     console.log(""),
     console.log("======================================"),
     startQuestions()
-  );
+);
 
 
 //   connection.query = util.promisify(connection.query);
 
 
 async function startQuestions() {
-    try{
+    try {
         const question = await inquirer.prompt([{
             type: 'list',
             name: 'choice',
             message: ('What would you like to do?'),
             choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Quit']
         }]);
-            switch (question.choice) {
-                case 'View all departments':
-                    return viewDepartments() ;
-                    break;
-                case 'View all roles':
-                    return viewRoles();
-                    break;
-                case 'View all employees':
-                    return viewEmployees();
-                    break;
-                case 'Add a department':
-                    return addDepartment();
-                    break;
-                    case 'Add a role':
-                    return addRole();
-                    break;
-                    case 'Add an employee':
-                    return addEmployees();
-                    break;
-                    case 'Update an employee role':
-                    return updateEmployees();
-                    break;
-                default:
-                    return quitIt();
-        
-            }
+        switch (question.choice) {
+            case 'View all departments':
+                return viewDepartments();
+                break;
+            case 'View all roles':
+                return viewRoles();
+                break;
+            case 'View all employees':
+                return viewEmployees();
+                break;
+            case 'Add a department':
+                return addDepartment();
+                break;
+            case 'Add a role':
+                return addRole();
+                break;
+            case 'Add an employee':
+                return addEmployees();
+                break;
+            case 'Update an employee role':
+                return updateEmployees();
+                break;
+            default:
+                return quitIt();
+
+        }
         // };
-    } catch(err) {
+    } catch (err) {
         console.log(err)
     };
 };
-        
-            //     {
-            //         type: 'input',
-            //         name: 'View all departments',
-            //         value: 'VIEW_DEPARTMENTS',
-            //     },
-            //     {
-            //         type: 'input',
-            //         name: 'View all roles',
-            //         value: 'VIEW_ROLES',
-            //     },
-            //     {
-            //         type: 'input',
-            //         name: 'View all managers',
-            //         value: 'VIEW_MANAGERS',
-            //     },
-            //     {
-            //         type: 'input',
-            //         name: 'add a department',
-            //         value: 'linkedIn',
-            //     },
-            //     {
-            //         type: 'input',
-            //         name: 'add a roles',
-            //         value: 'linkedIn',
-            //     },
-            //     {
-            //         type: 'input',
-            //         name: 'add a employee',
-            //         value: 'linkedIn',
-            //     },
-            //     {
-            //         type: 'input',
-            //         name: 'update an employee',
-            //         value: 'linkedIn',
-            //     },
-            //     {
-            //         name: 'Exit Program',
-            //         value: 'QUIT_IT',
-            //     },
-            // ]
-
-    // switch (start) {
-    //     case 'VIEW_DEPARTMENTS':
-    //         return viewDepartments();
-    //         break;
-    //     case 'VIEW_ROLES':
-    //         return viewRoles();
-    //         break;
-    //     case 'VIEW_':
-    //         return viewDepartments();
-    //         break;
-    //     case 'VIEW_ROLES':
-    //         return viewRoles();
-    //         break;
-    //     default:
-    //         return quitIt();
-
-    // }
-// };
 
 
-// VIEW DEPARTMENTS ------------------------
-///////// WHEN I choose to view all departments
-/////////THEN I am presented with a formatted table showing department names and department ids
 function viewDepartments() {
     connection.query("SELECT department.id AS ID, department.name AS Department FROM department",
-    function(err, res) {
-      if (err) throw err
-      console.log("")
-      console.log("*** DEPARTMENT LIST ***")
-      console.log("")
-      console.table(res)
-      startQuestions()
-  })
+        function (err, res) {
+            if (err) throw err
+            console.log("")
+            console.table(res)
+            console.log("^^^^^ DEPARTMENT LIST ABOVE ^^^^^")
+            startQuestions()
+        })
 }
 
-//WHEN I choose to view all roles
-//THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
 function viewRoles() {
-    connection.query("SELECT role.id AS ROLE_ID, role.title AS Title, role.salary AS Salary FROM role",
-    function(err, res) {
-      if (err) throw err
-      console.log("")
-      console.log("*** ROLE LIST ***")
-      console.log("")
-      console.table(res)
-      startQuestions()
-  })
+    connection.query("SELECT role.id AS ID, role.title AS Title, role.department_id AS Department, role.salary AS Salary FROM role",
+        function (err, res) {
+            if (err) throw err
+            console.log("")
+            console.table(res)
+            console.log("^^^^^ ROLE LIST ABOVE ^^^^^")
+            startQuestions()
+        })
 }
 
-// WHEN I choose to view all employees
-// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 
 function viewEmployees() {
-    connection.query("", 
-    function(err, res) {
-      if (err) throw err
-      console.log ("");
-      console.log("*** EMPLOYEES LIST BY DEPARTMENT ***")
-      console.log ("");
-      console.table(res)
-      startQuestions()
+    connection.query(`SELECT 
+    employee.id, 
+    employee.first_name, 
+    employee.last_name, 
+    CONCAT (employee.first_name, ' ', employee.last_name) 
+    as NAME,
+    role.title as ROLE, 
+    department.name as DEPARTMENT, 
+    CONCAT (manager.first_name, ' ', manager.last_name) 
+    AS MANAGER FROM employee 
+    LEFT JOIN role on employee.role_id = role.id 
+    LEFT JOIN department on role.department_id = department.id 
+    LEFT JOIN employee manager on employee.manager_id = manager.id`,
+        function (err, res) {
+            if (err) throw err
+            console.log("");
+            console.log("*** EMPLOYEES LIST BY DEPARTMENT ***")
+            console.log("");
+            console.table(res);
+            startQuestions()
+        })
+}
+
+
+
+function addDepartment() {
+
+    inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "What Department would you like to add? "
+        },
+        {
+            name: "id",
+            type: "input",
+            message: "What is the new Department ID number? "
+        }
+
+    ]).then(function (answers) {
+        connection.query("INSERT INTO department SET ? ",
+            {
+                name: answers.name,
+                id: answers.id
+            },
+            function (err) {
+                if (err) throw err
+
+              
+                    console.table(answers);
+                    console.log("====== ADDED ======")
+                startQuestions();
+            }
+
+        )
     })
-  }
+}
 
+// DEPARTMENT ARRAY SET UP FOR ROLE ADDITION __________________
+var deptArr = [];
+function selectDepartment() {
+    connection.query("SELECT * FROM department", function (err, res) {
+        if (err) throw err
+        for (var i = 0; i < res.length; i++) {
+            deptArr.push(res[i].name);
+        }
+    })
+    return deptArr;
+}
 
+// ADD ROLE __________________________________
+function addRole() {
+    connection.query("SELECT role.title AS Title, role.salary AS Salary FROM role LEFT JOIN department.name AS Department FROM department;", function (err, res) {
+        inquirer.prompt([
+            {
+                name: "title",
+                type: "input",
+                message: "What is name of the new role?"
+            },
+            {
+                name: "salary",
+                type: "input",
+                message: "What is the salary of the new role?"
+            },
+            {
+                name: "department",
+                type: "rawlist",
+                message: "Under which department does this new role fall?",
+                choices: selectDepartment()
+            }
+        ]).then(function (answers) {
+            var deptId = selectDepartment().indexOf(answers.choice) + 1
+            connection.query(
+                "INSERT INTO role SET ?",
+                {
+                    title: answers.title,
+                    salary: answers.salary,
+                    department_id: deptId
+                },
+                function (err) {
+                    if (err) throw err
+                    console.log("")
+                    console.table(answers);
+                    console.log("====== ADDED ======")
+                    startQuestions();
+                }
+            )
+        });
+    });
+};
 
-// async function viewDepartments() {
-//     const department = await db.viewAllDepartments();
-//     console.table(department);
-//     startQuestions();
+const addEmployees = () => {
+	const sqlQuery = 'INSERT INTO employee SET ?';
+	const roleQuery = 'SELECT * FROM role';
+	connection.query(roleQuery, function (error, results) {
+		let roles = [];
+		if (error) throw error;
+		roles = results.map(result => (
+            {
+			id: result.id,
+			title: result.title,
+	        }
+        ));
+
+		inquirer
+			.prompt([
+				{
+					type: 'input',
+					name: 'first_name',
+					message: 'Please, insert a employee first name',
+				},
+				{
+					type: 'input',
+					name: 'last_name',
+					message: 'Please, insert a employee last name',
+				},
+				{
+					type: 'list',
+					name: 'role_id',
+					message: 'What is the new employee role?',
+					choices: roles.map(role => ({
+						name: role.title,
+						value: role.id,
+					})),
+				}
+			])
+			.then(response => {
+				connection.query(sqlQuery, response, function (error, results) {
+					if (error) throw error;
+					console.log('Welcome the new employee!');
+					startQuestions();
+				});
+			});
+	});
+};
+
+// // UPDATE EMPLOYEE ROLE _______________________
+// // ROLE ARRAY SET UP FOR EMPLOYEE ADDITION _____________________
+// let roleArr = [];                                            
+// function selectRole() {
+//   connection.query("SELECT * FROM role", function(err, res) {
+//     if (err) throw err
+//     for (var i = 0; i < res.length; i++) {
+//       roleArr.push(res[i].title);
+//     }
+//   })
+//   return roleArr;
 // }
 
-
-// async function viewRoles() {
-//     const results = await db.promise().query(sql);
-
-//     console.table('', results[0]);
-//     startQuestions();
-// }
-
-// async function viewManager() {
-//     const manager = await db.viewAllManager();
-//     console.table(Manager);
-//     startQuestions();
-// }
+const updateEmployees = () => {
+	const sqlQuery = 'SELECT id, first_name, last_name, role_id FROM employee';
+	connection.query(sqlQuery, function (error, results) {
+		if (error) throw error;
+		const employees = results.map(employee => ({
+			name: `${employee.first_name} ${employee.last_name}`,
+			value: employee.id,
+		}));
+		employees.unshift({
+			name: 'None',
+			value: null,
+		});
+		inquirer
+			.prompt([
+				{
+					type: 'list',
+					name: 'id',
+					message: "Which employee's role do you want to update?",
+					choices: employees,
+				},
+			])
+			.then(({ id }) => {
+				const roleQuery = 'SELECT * FROM role';
+				connection.query(roleQuery, function (error, results) {
+					if (error) throw error;
+					console.log(results);
+					const roles = results.map(result => ({
+						name: result.title,
+						value: result.id,
+					})); inquirer
+                    .prompt([
+                        {
+                            type: 'list',
+                            name: 'role_id',
+                            message: 'Which is the new role for this employee employee?',
+                            choices: roles,
+                        },
+                    ])
+                    .then(({ role_id }) => {
+                        const updateQuery = `UPDATE employee SET ? WHERE id = ${id}`;
+                        connection.query(updateQuery, { role_id }, function (error, results) {
+                            if (error) throw error;
+                            console.log("Updated employee's role");
+                            startQuestions();
+                        });
+                    });
+            });
+        });
+});
+};
 
 function quitIt() {
-    console.log (""),
-    console.log ("======================================"),
-    console.log (""),
-    console.log ("   LOGGING OUT - HAVE A GREAT SHIFT   "),
-    console.log (""),
-    console.log ("======================================"),
-    process.exit();
+    console.log(""),
+        console.log("======================================"),
+        console.log(""),
+        console.log("   LOGGING OUT - HAVE A GREAT SHIFT   "),
+        console.log(""),
+        console.log("======================================"),
+        process.exit();
 }
 
